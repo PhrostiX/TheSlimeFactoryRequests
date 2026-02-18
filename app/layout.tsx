0108ba5f6876f79826d23b56d45a4eaffcc28769
PhrostiX <owenleehome@yahoo.com>
2026-02-18 00:08:09 -0700

    darkmode

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <html lang="en" className="dark">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <div className="layoutRoot">
           {children}
           <FooterBar />
